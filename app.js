f90c7124bdaaf168f954ec735e222a44719608bd
unknown <carlosgomez0717@gmail.com>
2024-08-18 19:39:59 -0500

    Nueva Api Rest React

--- app.js
+++ app.js
@@ -2,19 +2,27 @@ const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
 const userRoute = require("./routes/user");
+var cors = require('cors')
+
+
 
 // settings
 const app = express();
 const port = process.env.PORT || 3000;
 
 // middlewares
+app.use(cors())
 app.use(express.json());
 app.use("/api", userRoute);
+
+
 
 // routes
 app.get("/", (req, res) => {
   res.send("Welcome to my API");
-});
+ 
+ });
+
 
 // mongodb connection
 mongoose
